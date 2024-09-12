c1d8d4769c6c476ed1fb456c8e0ef311c83f86a7
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-09-12 12:19:40 +0700

    Locomotive Scroll for smoothness.

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,41 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
 import Projects from "./sections/Projects";
 import Experience from "./sections/Experience";
+import "locomotive-scroll/dist/locomotive-scroll.css";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    let locomotiveScroll;
+
+    (async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+
+      locomotiveScroll = new LocomotiveScroll({
+        el: document.querySelector("[data-scroll-container]"),
+        smooth: true,
+        smartphone: { smooth: true },
+        tablet: { smooth: true },
+      });
+    })();
+
+    setTimeout(() => {
+      setIsLoading(false);
+      document.body.style.cursor = "default";
+      window.scrollTo(0, 0);
+    }, 2000);
+
+    return () => {
+      if (locomotiveScroll) locomotiveScroll.destroy();
+    };
+  }, []);
+
   return (
-    <main className="mx-auto">
+    <main className="mx-auto" data-scroll-container>
       <Navbar />
       <Hero />
       <div className="md:px-10">
