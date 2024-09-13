5cb532b74fef65c097cde5ddd8913c2fa08d8744
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-09-13 20:24:16 +0700

    no padding on main?

--- app/page.jsx
+++ app/page.jsx
@@ -58,11 +58,9 @@ const App = () => {
       </div>
       <ShrineSection />
 
-      <div className="md:px-10">
-        <About />
-        <Projects />
-        <Experience />
-      </div>
+      <About />
+      <Projects />
+      <Experience />
     </main>
   );
 };
