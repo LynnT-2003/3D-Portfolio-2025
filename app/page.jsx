5c32d8cc61e49872a910f77092d3c58fe51eb4a4
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-09-13 20:01:15 +0700

    ok

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import Navbar from "./sections/Navbar";
+// import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
 // import Projects from "./sections/Projects";
 // import Experience from "./sections/Experience";
