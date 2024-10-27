5ce502413290ef3205b2767b45b00cb68dd5154f
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-10-27 08:52:39 +0700

    Update other showcases section.

--- app/sections/Showcase.jsx
+++ app/sections/Showcase.jsx
@@ -3,21 +3,27 @@ import React from "react";
 const Showcase = () => {
   return (
     <div className="pb-24 text-white">
-      <p className="head-text mt-24 mb-12 ml-20">Other Showcases</p>
+      <p className="head-text mt-24 mb-12 ml-[5vw]">Other Showcases</p>
       <div className="w-screen flex flex-col items-center justify-center">
         <div className="w-[90vw] space-x-12 flex flex-row">
-          <div className="w-1/2 flex items-center justify-center">
-            <video className="w-full h-auto" controls autoPlay loop muted>
-              <source
-                src="/textures/project/mv-showcase.mp4"
-                type="video/mp4"
-              />
+          <div className="w-1/2">
+            <video
+              className="w-full max-w-[800px] h-[450px] object-cover"
+              style={{ aspectRatio: "16/9" }}
+              controls
+              autoPlay
+              loop
+              muted
+            >
+              <source src="others/emotion.mp4" type="video/mp4" />
               Your browser does not support the video tag.
             </video>
           </div>
 
           <div className="w-1/2 flex flex-col">
-            <p className="text-2xl mb-6">Maha Vertex Web Platform</p>
+            <p className="text-4xl font-bold font-sans text-gray-400 mb-6">
+              Hugging Face Emotion Classifier
+            </p>
             <p className="leading-loose spacing-loose">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
               ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum
@@ -25,6 +31,36 @@ const Showcase = () => {
               amet consectetur adipisicing elit. Lorem ipsum dolor sit amet
               consectetur adipisicing elit.
             </p>
+          </div>
+        </div>
+        <div className="w-[90vw] h-screen mt-64 space-x-12 flex flex-row">
+          <div className="w-1/2 flex flex-col">
+            <p className="text-4xl mt-1 font-bold font-sans text-gray-400 mb-6">
+              Gesture Controlled Catch Game in NextJS
+            </p>
+            <p className="leading-loose spacing-loose">
+              An interactive web game utilizing TensorFlow.js and the Handpose
+              model, allowing players to control a basket via hand gestures to
+              catch randomized falling objects. Everything, including the object
+              and animations, are built using pure Javascript. Real-time hand
+              tracking aligns the basket with the player’s movements, while
+              randomized visuals keep gameplay engaging. This project highlights
+              advanced computer vision applications, responsive interactions,
+              and creative design for an immersive experience.
+            </p>
+          </div>
+          <div className="w-1/2">
+            <video
+              className="w-full max-w-[800px] h-[450px] object-cover"
+              style={{ aspectRatio: "16/9" }}
+              controls
+              autoPlay
+              loop
+              muted
+            >
+              <source src="others/hand.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
           </div>
         </div>
       </div>
